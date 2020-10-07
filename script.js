d9be2fb9304b75cf1324aaa6bb6729317e1ef905
Vladislav110 <70592682+Vladislav110@users.noreply.github.com>
2020-10-07 22:31:44 +0300

    fix: change layout page Singolo

--- script.js
+++ script.js
@@ -35,47 +35,73 @@ function showSlides(n) {
 let portfolioTages = document.querySelector('.image_style');
 let portfolio = document.querySelector('.section2 .portfolio_image');
 let portfolioImeges = portfolio_image.querySelectorAll('IMG');
-let arr =[];
+let arr = [];
 
 portfolioTages.addEventListener('click', (Event) => {
     if (!event.target.classList.contains('icon_all')) {
-    portfolioTages.querySelectorAll('image_position').forEach( item => item.classList.remove('icon_all'));
-    Event.target.classList.add('icon_all');
-    shuffleImages();
-}
+        portfolioTages.querySelectorAll('image_position').forEach(item => item.classList.remove('icon_all'));
+        Event.target.classList.add('icon_all');
+        shuffleImages();
+    }
 });
 
-portfolioImeges.forEach( (item) => {
+portfolioImeges.forEach((item) => {
     arr.push(item);
     item.addEventListener('click', () => {
-        portfolioImeges.forEach( item => item.classList.remove('IMG'));
+        portfolioImeges.forEach(item => item.classList.remove('IMG'));
         Event.target.classList.add('IMG');
     });
 });
 
 function shuffleImages() {
-    portfolioImeges.forEach (item => item.classList.remove('IMG'));
+    portfolioImeges.forEach(item => item.classList.remove('IMG'));
 
     while (portfolio.firstChild) {
         portfolio.removeChild(portfolio.firstChild);
     }
 
-let currentIndex = portfolioImeges.length;
-let temporaryValue, randomIndex;
+    let currentIndex = portfolioImeges.length;
+    let temporaryValue, randomIndex;
 
-while(currentIndex !==0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -=1;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    temporaryValue = arr[currentIndex];
-    arr[currentIndex] = arr[randomIndex];
-    arr[randomIndex] = temporaryValue;
+        temporaryValue = arr[currentIndex];
+        arr[currentIndex] = arr[randomIndex];
+        arr[randomIndex] = temporaryValue;
+    }
+
+    arr.forEach(item => {
+        item.classList.add('hidden');
+        portfolio.appendChild(item);
+        setTimeout(() => item.classList.remove('hidden'), 300);
+    });
 }
 
-arr.forEach(item => {
-    item.classList.add('hidden');
-    portfolio.appendChild(item);
-    setTimeout(() => item.classList.remove('hidden'),300);
-});
-}
+/*
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.menu__btn');
+    let links = menu.find('.menu__item');
+    let overlay = menu.find('.burger_menu_overlay');
 
+    button.on('click', (e) => {
+        e.preventDefult();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu() {
+        menu.toggleClass('burger_menu_active');
+
+        if (menu.hasClass('burger_menu_active')) {
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'visible');
+        }
+    }
+}
+burgerMenu('.hamburger-menu');
